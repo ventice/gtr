799ca40087e0b66bca1e7175625b1531150c0130
venticello <omnisens@gmail.com>
2025-11-27 14:44:57 +0400

    Use prompt version 3

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 PROMPT_ID = "pmpt_6860f7f59b60819594aab976befdbae20bd70b02750bfd8e"
-PROMPT_VERSION = "2"
+PROMPT_VERSION = "3"
 BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 APP_URL = os.environ["APP_URL"]
